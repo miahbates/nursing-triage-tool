89b82c3aadbae507ec3af4ef23114362fa3e45bc
miahbates <miahbates@icloud.com>
2021-10-13 22:09:34 +0100

    js for removing intro when button clicked

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello");
+// grab button 
+const triageButton = document.getElementById("triageButton");
+
+// grab header
+const intro = document.getElementsByClassName("intro")[0];
+
+// when clicked hide header info
+triageButton.addEventListener("click", function hideInto() {
+  intro.style.visibility = "hidden";
+})
+
+console.log("hello world");
