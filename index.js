9fc27ccb96abee1e21809f17f33f6af0e7935bc2
miahbates <miahbates@icloud.com>
2021-10-24 09:31:39 +0100

    symptoms-nav

--- index.js
+++ index.js
@@ -14,4 +14,27 @@ triageButton.addEventListener("click", function hideIntro() {
 })
 
 
+// switching between questions
+
+let buttons = document.querySelectorAll('[data-to]');
+const sections = document.querySelectorAll('[data-index]'); 
+
+for (let i = 0; i < buttons.length; i++ ) {
+  buttons[i].addEventListener('click', () => {
+    // loop over sections and hide them all
+    for (let j = 0; j < sections.length; j++) {
+      sections[j].setAttribute('hidden');
+    }
+    // on the button clicked, find the target index to show
+      let target = buttons[i].dataset.to;
+      // query select to get the thing with [data-index="TARGET"]
+      let matchingTarget = document.querySelector([`data-index=${target}`])
+      // show that section
+      matchingTarget.removeAttribute(hidden);
+  })
+}
+
+//data set find value read only
+//query selector grab element 
+
 console.log("hello world");
