197451b54944be0a0809baa7746d3982e83886f1
miahbates <miahbates@icloud.com>
2021-12-15 13:07:49 +0000

    erestart assessment button completed removed hidden attribute from intro and looped over restart buttton to find out which is clicked and hide all sections

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// Hide into button and show symptom questions.
-
+// Hide intro button and show symptom questions.
 // grab button 
 const triageButton = document.getElementById("triageButton");
 
@@ -13,11 +12,9 @@ const symptoms = document.querySelector(".symptoms");
 triageButton.addEventListener("click", function hideIntro() {
   intro.setAttribute('hidden', true);
   symptoms.removeAttribute("hidden");
-})
-
+});
 
 // Switching between symptom questions
-
 // Access the buttons clicked and section information using data attributes and store them in variables.
 let buttons = document.querySelectorAll('[data-to]');
 const sections = document.querySelectorAll('[data-index]'); 
@@ -30,15 +27,62 @@ for (let i = 0; i < buttons.length; i++ ) {
       sections[j].setAttribute('hidden', true);
     }
     // on the button clicked, find the target index to show
-      let target = buttons[i].dataset.to;
-      // query select to get the thing with [data-index=2]
-      let matchingTarget = document.querySelector(`[data-index="${target}"]`)
-      // show that section
-      matchingTarget.removeAttribute('hidden');
-  })
+    let target = buttons[i].dataset.to;
+    // query select to get the thing with [data-index=2]
+    let matchingTarget = document.querySelector(`[data-index="${target}"]`)
+    // show that section
+    matchingTarget.removeAttribute('hidden');
+  });
 }
 
 let patientScore = 0;
+
+let nextButtons = document.querySelectorAll('[data-name]');
+
+nextButtons.forEach(nextButton => {
+  nextButton.addEventListener('click', () => {
+    let name = nextButton.dataset.name;
+    let radioButtons = document.querySelectorAll(`input[name='${name}']`);
+    radioButtons.forEach(radio => { 
+      if (radio.checked) {
+        patientScore += parseInt(radio.value);
+      }
+    });
+    // console.log('Your score is ', patientScore);
+  });
+});
+
+// Restart assessment 
+// get all buttons
+const restartAssessmentBtns = document.querySelectorAll('.restartAssessment');
+// console.log(restartAssessmentBtn);
+
+// restartAssessmentBtns.addEventListener('click', () => {
+//   for (let i = 0; i < sections.length; i++) {
+//     console.log(sections);
+//     sections[i].setAttribute('hidden', true);
+//   };
+//   intro.setAttribute('hidden', false);
+//   patientScore = 0;
+// });
+
+for (let i = 0; i < restartAssessmentBtns.length; i++ ) {
+  restartAssessmentBtns[i].addEventListener('click', (e) => {
+    e.preventDefault();
+    // loop over sections and hide them all
+    for (let j = 0; j < sections.length; j++) {
+      sections[j].setAttribute('hidden', true);
+    }
+    intro.removeAttribute("hidden");
+    patientScore = 0;
+  });
+}
+
+// make current page hidden 
+
+// and show intro 
+
+// set patient score to 0
 
 // let feverButton = document.getElementById('submit-fever');
 
@@ -52,29 +96,11 @@ let patientScore = 0;
 
 // })
 
-let nextButtons = document.querySelectorAll('[data-name]');
-
-nextButtons.forEach(nextButton => {
-  nextButton.addEventListener('click', () => {
-    let name = nextButton.dataset.name;
-    let radioButtons = document.querySelectorAll(`input[name='${name}']`);
-    radioButtons.forEach(radio => { 
-      if (radio.checked) {
-        patientScore += parseInt(radio.value);
-      }
-    })
-    console.log('Your score is ', patientScore);
-  })
-})
-
 // Go to waiting room page
 // loop over scores to see which is highest and prioritise accordingly.
-
 
 // seperate event listeners which find target info from questions and push to an array
 // one event listener which listens to what form was submit
 
 // Data set find value - read only
-// Query selector grab elements 
-
-console.log("hello world");
+// Query selector grab elements
